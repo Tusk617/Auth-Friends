dc5269e95fabbc614f57f53742901cfd3a4f7949
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-21 15:41:34 -0400

    Created a login page component and was able to import it to App.js and render it. Also imported and wrapped app in router

---
+++ friends/src/components/LoginPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export const Login = () => {
+    return (
+        <div>
+            <input 
+                type="text"
+                name="username"
+                placeholder="Username:"
+            
+            />
+            <input 
+                type="password"
+                name="password"
+                placeholder="Password:"
+            
+            
+            />
+        </div>
+    )
+}
